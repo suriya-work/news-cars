f3e479333ed6057e61f92aff7a498332ca645bff
suriya babaei <suriyalifework60@gmail.com>
2023-04-05 08:53:34 -0700

    create carPages

--- components/templates/CarsPage.js
+++ components/templates/CarsPage.js
@@ -1,10 +1,11 @@
+import Card from '../module/Card';
 import styles from './CarsPage.module.css'
 const CarsPage = ({ data }) => {
     return (
 
         <div className={styles.container}>
             {
-                data.map((car) => <p key={car.id}>{car.name}</p>)
+                data.map((car) => <Card  key={car.id} {...car}/>)
             }
         </div>
     );
