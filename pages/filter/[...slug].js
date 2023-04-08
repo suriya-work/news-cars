a839129a7111b998b9b83cd57c601cb4d5f7f348
suriya babaei <suriyalifework60@gmail.com>
2023-04-08 07:54:21 -0700

    index slug

--- pages/filter/[...slug].js
+++ pages/filter/[...slug].js
@@ -1,13 +1,18 @@
 import { useRouter } from "next/router";
 import carsData from "@/data/carsData";
 import CarsList from "@/components/templates/CarsList";
-
-const FilterCars = ({data}) => {
+function FilteredCars() {
     const router = useRouter();
-    const [min, max] = router.query.slug || [];
-    const filteredData = carsData.filter((item) => item.price > min && item.price < max);
-    if (!filteredData.length) return <h3>NotFound</h3>
-    return <CarsList data={filteredData} />
-};
 
-export default FilterCars;
+    const [min , max] = router.query.slug || [];
+
+    const filteredData = carsData.filter(
+        (item) => item.price > min && item.price < max
+    );
+
+    if (!filteredData.length) return <h3>NotFound</h3>;
+
+    return <CarsList data={filteredData} />;
+}
+
+export default FilteredCars;
