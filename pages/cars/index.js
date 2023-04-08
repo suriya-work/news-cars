bf51cbf0bc92a3563f939137810dda66bbe28d83
suriya babaei <suriyalifework60@gmail.com>
2023-04-08 02:12:29 -0700

    index

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,9 +1,11 @@
 import carsData from "@/data/carsData";
 import CarsPage from "@/components/templates/CarsPage";
 import Categories from "@/components/module/Categories";
+import SearchBar from "@/components/module/SearchBar";
 const Detailes = () => {
     return (
         <div>
+            <SearchBar />
             <Categories />
             <CarsPage data={carsData} />
         </div>
