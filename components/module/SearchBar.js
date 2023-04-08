1338d24c447909ead26ddc9648150d08320d00ac
suriya babaei <suriyalifework60@gmail.com>
2023-04-08 02:11:43 -0700

    create searchbar and styles

---
+++ components/module/SearchBar.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import styles from "./SearchBar.module.css";
+import { useRouter } from "next/router";
+const SearchBar = () => {
+    const router = useRouter();
+
+    const [min, setMin] = useState("");
+    const [max, setMax] = useState("");
+    const searchHandelr = () => {
+        if (min && max) {
+            router.push(`/filter ${min} ${max}`)
+        } else {
+            alert('Please Enter minimum and maximum price!')
+        }
+    }
+    return (
+        <div className={styles.container}>
+            <div>
+                <input placeholder="Inter min-price" value={min} onChange={e => setMin(e.target.value)} />
+                <input placeholder="Inter max-price" value={max} onChange={e => setMax(e.target.value)} />
+            </div>
+            <button onClick={searchHandelr}>Search</button>
+        </div>
+    );
+};
+
+export default SearchBar;
